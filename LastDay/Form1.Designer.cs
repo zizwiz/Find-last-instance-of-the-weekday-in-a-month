3ae2c626bdf38cb65db67352d233e1d98d0be00b
ziz wiz <zizwiz@engineer.com>
2023-07-27 20:05:51 +0100

    Started to add in a check to tell us if UK is in winter or Summer time.

--- LastDay/Form1.Designer.cs
+++ LastDay/Form1.Designer.cs
@@ -37,13 +37,26 @@ namespace LastDay
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.cmbobx_day = new System.Windows.Forms.ComboBox();
+            this.lbl_season_time = new System.Windows.Forms.Label();
+            this.lbl_season_hours = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtTmPicker = new System.Windows.Forms.DateTimePicker();
+            this.btn_time_in_use_in_UK = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_date_to_change_to_BST = new System.Windows.Forms.Label();
+            this.lbl_date_to_change_to_GMT = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // rchtxtbx_output
             // 
             this.rchtxtbx_output.Location = new System.Drawing.Point(43, 41);
             this.rchtxtbx_output.Name = "rchtxtbx_output";
-            this.rchtxtbx_output.Size = new System.Drawing.Size(999, 569);
+            this.rchtxtbx_output.Size = new System.Drawing.Size(783, 591);
             this.rchtxtbx_output.TabIndex = 0;
             this.rchtxtbx_output.Text = "";
             // 
@@ -178,11 +191,137 @@ namespace LastDay
             this.cmbobx_day.Size = new System.Drawing.Size(140, 28);
             this.cmbobx_day.TabIndex = 5;
             // 
+            // lbl_season_time
+            // 
+            this.lbl_season_time.AutoSize = true;
+            this.lbl_season_time.Location = new System.Drawing.Point(26, 411);
+            this.lbl_season_time.Name = "lbl_season_time";
+            this.lbl_season_time.Size = new System.Drawing.Size(155, 20);
+            this.lbl_season_time.TabIndex = 8;
+            this.lbl_season_time.Text = "British Summer Time";
+            // 
+            // lbl_season_hours
+            // 
+            this.lbl_season_hours.AutoSize = true;
+            this.lbl_season_hours.Location = new System.Drawing.Point(26, 438);
+            this.lbl_season_hours.Name = "lbl_season_hours";
+            this.lbl_season_hours.Size = new System.Drawing.Size(164, 20);
+            this.lbl_season_hours.TabIndex = 9;
+            this.lbl_season_hours.Text = "Time in GMT + 1 hour ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lbl_date_to_change_to_GMT);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lbl_date_to_change_to_BST);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btn_time_in_use_in_UK);
+            this.groupBox1.Controls.Add(this.dtTmPicker);
+            this.groupBox1.Controls.Add(this.lbl_season_hours);
+            this.groupBox1.Controls.Add(this.lbl_season_time);
+            this.groupBox1.Location = new System.Drawing.Point(855, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 591);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Time in use in UK on";
+            // 
+            // dtTmPicker
+            // 
+            this.dtTmPicker.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dtTmPicker.CustomFormat = "dd-MM-yyyy";
+            this.dtTmPicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtTmPicker.Location = new System.Drawing.Point(27, 38);
+            this.dtTmPicker.Name = "dtTmPicker";
+            this.dtTmPicker.Size = new System.Drawing.Size(151, 26);
+            this.dtTmPicker.TabIndex = 10;
+            this.dtTmPicker.Value = new System.DateTime(2023, 7, 22, 21, 44, 0, 0);
+            // 
+            // btn_time_in_use_in_UK
+            // 
+            this.btn_time_in_use_in_UK.Location = new System.Drawing.Point(22, 526);
+            this.btn_time_in_use_in_UK.Name = "btn_time_in_use_in_UK";
+            this.btn_time_in_use_in_UK.Size = new System.Drawing.Size(159, 47);
+            this.btn_time_in_use_in_UK.TabIndex = 11;
+            this.btn_time_in_use_in_UK.Text = "Find Answer";
+            this.btn_time_in_use_in_UK.UseVisualStyleBackColor = true;
+            this.btn_time_in_use_in_UK.Click += new System.EventHandler(this.btn_time_in_use_in_UK_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Change to BST:";
+            // 
+            // lbl_date_to_change_to_BST
+            // 
+            this.lbl_date_to_change_to_BST.AutoSize = true;
+            this.lbl_date_to_change_to_BST.Location = new System.Drawing.Point(26, 133);
+            this.lbl_date_to_change_to_BST.Name = "lbl_date_to_change_to_BST";
+            this.lbl_date_to_change_to_BST.Size = new System.Drawing.Size(53, 20);
+            this.lbl_date_to_change_to_BST.TabIndex = 13;
+            this.lbl_date_to_change_to_BST.Text = "Date1";
+            // 
+            // lbl_date_to_change_to_GMT
+            // 
+            this.lbl_date_to_change_to_GMT.AutoSize = true;
+            this.lbl_date_to_change_to_GMT.Location = new System.Drawing.Point(26, 267);
+            this.lbl_date_to_change_to_GMT.Name = "lbl_date_to_change_to_GMT";
+            this.lbl_date_to_change_to_GMT.Size = new System.Drawing.Size(53, 20);
+            this.lbl_date_to_change_to_GMT.TabIndex = 15;
+            this.lbl_date_to_change_to_GMT.Text = "Date2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Change to GMT:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 376);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Chosen Date is in:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "2am becomes 1am";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(144, 20);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "1am becomes 2am";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 701);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cmbobx_day);
             this.Controls.Add(this.label1);
@@ -196,6 +335,8 @@ namespace LastDay
             this.Name = "Form1";
             this.Text = "Last instance of day of week in the month";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -210,6 +351,18 @@ namespace LastDay
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cmbobx_day;
+        private System.Windows.Forms.Label lbl_season_time;
+        private System.Windows.Forms.Label lbl_season_hours;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtTmPicker;
+        private System.Windows.Forms.Button btn_time_in_use_in_UK;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_date_to_change_to_GMT;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_date_to_change_to_BST;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- LastDay/Form1.cs
+++ LastDay/Form1.cs
@@ -19,37 +19,88 @@ namespace LastDay
         {
             Close();
         }
-        
+
         private void Form1_Load(object sender, EventArgs e)
         {
             Text += " : v" + Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
 
             //set the day of week to present day
-            string[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
             DayOfWeek currentDayOfWeek = DateTime.Now.DayOfWeek;
             cmbobx_day.SelectedIndex = Array.IndexOf(daysOfWeek, currentDayOfWeek.ToString());
 
             // Set year to 2023.
             cmbobx_year.SelectedIndex = 23;
+
+            // set date picker to todays date.
+            dtTmPicker.Value = DateTime.Today;
         }
-        
+
         private void btn_go_Click(object sender, EventArgs e)
+        {
+            workOutDates(12);
+        }
+
+        private void workOutDates(int months)
         {
             rchtxtbx_output.Clear();
 
+            bool flag = false;
             int year = Convert.ToInt32(cmbobx_year.Text);
             var myDayOfWeek = (DayOfWeek)(cmbobx_day.SelectedIndex + 1);
-
+            int month;
             DateTime date;
+            string answer;
 
-            for (int i = 1; i <= 12; i++) // 12 for num months in year
+            for (int i = 1; i <= months; i++) 
             {
-                date = new DateTime(year, i, DateTime.DaysInMonth(year, i), System.Globalization.CultureInfo.CurrentCulture.Calendar);
+                if (flag) //if we want all the months or just when we change to and from BST
+                {
+                    month = (months == 2) ? 10 : i;
+                }
+                else
+                {
+                    month = (months == 2) ? 3 : i; 
+                }
 
-                int daysOffset = date.DayOfWeek - myDayOfWeek; 
+                myDayOfWeek = (months == 2)? DayOfWeek.Sunday : myDayOfWeek; //clocks change on a Sunday
+
+               date = new DateTime(year, month, DateTime.DaysInMonth(year, month), System.Globalization.CultureInfo.CurrentCulture.Calendar);
+
+                int daysOffset = date.DayOfWeek - myDayOfWeek;
                 if (daysOffset < 0) daysOffset += 7; // if the code is negative, we need to normalize them
-                rchtxtbx_output.AppendText(date.AddDays(-daysOffset).ToLongDateString() + "\r"); // write days to the richtextbox.
+
+                answer = date.AddDays(-daysOffset).ToLongDateString();
+
+                rchtxtbx_output.AppendText(answer + "\r"); // write days to the richtextbox.
+
+                if ((months == 2) && (flag))
+                {
+                    lbl_date_to_change_to_GMT.Text = answer;
+                }
+                else if ((months == 2) && (!flag))
+                {
+                    lbl_date_to_change_to_BST.Text = answer;
+                }
+
+                flag = true;
             }
+        }
+
+        private void btn_time_in_use_in_UK_Click(object sender, EventArgs e)
+        {
+            workOutDates(2);
+
+            DateTime dateToCheck = dtTmPicker.Value;
+            //DateTime startDate = DateTime.ParseExact(lbl_date_to_change_to_BST.Text, "dd-mmm-yyyy");
+            //DateTime endDate = (DateTime)lbl_date_to_change_to_GMT.Text;
+
+            //DateTime.ParseExact("2009-05-08 14:40:52,531", "yyyy-MM-dd HH:mm:ss,fff",
+
+            //bool isBST = (dateToCheck >= startDate && dateToCheck <= endDate);
+
+
+
         }
     }
 }
